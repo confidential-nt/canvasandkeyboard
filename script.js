1fca05c7eb8211b512c38c9cf385c76987071e43
confidential-nt <54153087+confidential-nt@users.noreply.github.com>
2020-10-07 00:01:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,17 @@ canvas.height = 700;
 canvas.strokeStyle = "black";
 canvas.fillStyle = "blue";
 
+let count = 0;
+
 ctx.rect(10, 20, 30, 40);
 ctx.stroke();
 ctx.fill();
 
-function hanldeCanvasLeft() {
+function hanldeCanvasLeft(num) {
   ctx.clearRect(10, 20, 30, 40);
   ctx.fillStyle = "black";
   ctx.strokeStyle = "black";
-  ctx.rect(5, 20, 30, 40);
+  ctx.rect(2 * num, 20, 30, 40);
   ctx.stroke();
   ctx.fill();
 }
@@ -31,10 +33,10 @@ function handleCanvasRight(num) {
 
 function detectWhichKey(event) {
   if (event.keyCode == 39) {
-    let count = 0;
-    count++;
-    handleCanvasRight(count++);
+    count = count + 1;
+    handleCanvasRight(count);
   } else if (event.keyCode == 37) {
-    hanldeCanvasLeft();
+    count = count + 1;
+    hanldeCanvasLeft(count);
   } else return;
 }

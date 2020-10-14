6e840fa905716036a7c5337e64899b23470759f3
confidential-nt <54153087+confidential-nt@users.noreply.github.com>
2020-10-15 03:41:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,42 +14,6 @@ ctx.rect(10, 20, 30, 40);
 ctx.stroke();
 ctx.fill();
 
-// function handleCanvasUp(num) {
-//   ctx.clearRect(10, 20, 30, 40);
-//   ctx.fillStyle = "black";
-//   ctx.strokeStyle = "black";
-//   ctx.rect(10, 20 - num, 30, 40);
-//   ctx.stroke();
-//   ctx.fill();
-// }
-
-// function handleCanvasDown(num) {
-//   ctx.clearRect(10, 20, 30, 40);
-//   ctx.fillStyle = "black";
-//   ctx.strokeStyle = "black";
-//   ctx.rect(10, 20 + num, 30, 40);
-//   ctx.stroke();
-//   ctx.fill();
-// }
-
-// function hanldeCanvasLeft(num) {
-//   ctx.clearRect(10, 20, 30, 40);
-//   ctx.fillStyle = "black";
-//   ctx.strokeStyle = "black";
-//   ctx.rect(10 - num, 20, 30, 40);
-//   ctx.stroke();
-//   ctx.fill();
-// }
-
-// function handleCanvasRight(num) {
-//   ctx.clearRect(10, 20, 30, 40);
-//   ctx.fillStyle = "black";
-//   ctx.strokeStyle = "black";
-//   ctx.rect(10 + num, 20, 30, 40);
-//   ctx.stroke();
-//   ctx.fill();
-// }
-
 function handleCanvas(num1,num2){
     ctx.clearRect(10, 20, 30, 40);
   ctx.fillStyle = "black";
@@ -62,28 +26,28 @@ function handleCanvas(num1,num2){
 let direction = [];
 
 function detectWhichKey(event) {
-  if (event.keyCode == 39 || event.keyCode == 37) {
+  if (event.keyCode == 39) {
     countX = countX+ 1;
     direction[0] = countX;    
   }
-  if(event.keyCode == 40 || event.keyCode == 38){
+  if(event.keyCode == 37){
+    countX = countX -1;
+    direction[0] = countX;
+  }
+  if(event.keyCode == 38){
+    countY = countY -1;
+    direction[1] = countY;
+    
+  }
+  if(event.keyCode == 40){
     countY = countY + 1;
     direction[1] = countY;//마이너스조건 만들기
   }
   const [x,y] = direction;
   handleCanvas(x,y);
- 
 }
   
 
-//   handleCanvasRight(countX);
-  // } else if () {
-  //   count = count + 1;
-  //   handleCanvasDown(count);
-  // } else if (event.keyCode == 38) {
-  //   count = count + 1;
-  //   handleCanvasUp(count);
-  // } else return;
 
 
 //한꺼번에
